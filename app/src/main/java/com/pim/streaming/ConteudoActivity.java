04dadfee200cb1f4364c72baf865cc5d556fcd2c
Vitor Araujo <vitorgamer58@outlook.com>
2024-11-23 10:13:05 -0300

    Adiciona botão de curtir

--- app/src/main/java/com/pim/streaming/ConteudoActivity.java
+++ app/src/main/java/com/pim/streaming/ConteudoActivity.java
@@ -1,6 +1,8 @@
 package com.pim.streaming;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,5 +24,14 @@ public class ConteudoActivity extends AppCompatActivity {
         TextView descricaoView = findViewById(R.id.descricaoConteudo);
         tituloView.setText(titulo);
         descricaoView.setText(descricao);
+
+        Button botaoCurtir = findViewById(R.id.butaoCurtir);
+
+        botaoCurtir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                botaoCurtir.setText("Curtiu");
+            }
+        });
     }
 }
